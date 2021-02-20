d57d04f9fcc7c28fc911ce7bfbcbf2cf915b0951
Rashid <rashid.cdac@gmail.com>
2021-02-20 21:54:31 +0530

    Commit to service-2

---
+++ movie-info-service/src/test/java/ra/tech/concepts/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.tech.concepts.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
